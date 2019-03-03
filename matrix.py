547ccd104087f82add10f349b9bb7785b3705c79
Anton Danylenko <adanylenko@stuy.edu>
2019-03-03 18:26:42 -0500

    s

--- matrix.py
+++ matrix.py
@@ -29,30 +29,30 @@ def make_rotX( theta ):
     matrix = new_matrix()
     radians = theta / 180 * math.pi
     ident(matrix)
-    matrix[1][1] = math.cos(radians)
-    matrix[1][2] = 0-math.sin(radians)
-    matrix[2][1] = math.sin(radians)
-    matrix[2][2] = math.cos(radians)
+    matrix[1][1] = int(math.cos(radians))
+    matrix[1][2] = int(0-math.sin(radians))
+    matrix[2][1] = int(math.sin(radians))
+    matrix[2][2] = int(math.cos(radians))
     return matrix
 
 def make_rotY( theta ):
     matrix = new_matrix()
     radians = theta / 180 * math.pi
     ident(matrix)
-    matrix[0][0] = math.cos(radians)
-    matrix[0][2] = math.sin(radians)
-    matrix[2][0] = 0-math.sin(radians)
-    matrix[2][2] = math.cos(radians)
+    matrix[0][0] = int(math.cos(radians))
+    matrix[0][2] = int(math.sin(radians))
+    matrix[2][0] = int(0-math.sin(radians))
+    matrix[2][2] = int(math.cos(radians))
     return matrix
 
 def make_rotZ( theta ):
     matrix = new_matrix()
     radians = theta / 180 * math.pi
     ident(matrix)
-    matrix[0][0] = math.cos(radians)
-    matrix[0][1] = 0-math.sin(radians)
-    matrix[1][0] = math.sin(radians)
-    matrix[1][1] = math.cos(radians)
+    matrix[0][0] = int(math.cos(radians))
+    matrix[0][1] = int(0-math.sin(radians))
+    matrix[1][0] = int(math.sin(radians))
+    matrix[1][1] = int(math.cos(radians))
     return matrix
 
 #print the matrix such that it looks like
