be80f49dcd8a8d56e6478b1164795f67e819da6b
Anton Danylenko <adanylenko@stuy.edu>
2019-03-03 23:48:43 -0500

    s

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ transform = new_matrix()
 
 #parse_file( 'script', edges, transform, screen, color )
 
-global DEFAULT_COLOR
+del DEFAULT_COLOR
 DEFAULT_COLOR = [184,149,75]
 screen2 = new_screen( )
 color3 = [0,0,0]
